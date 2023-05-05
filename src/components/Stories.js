f02af1eb6fe48de7de56670a2ce5ca514dbe7e48
thiago-arend <thiago.arend@gmail.com>
2023-05-05 10:21:36 -0300

    otimização renderização dos stories com map

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,16 +1,18 @@
 import Story from "./Story";
 
 export default function () {
+    const stories = [{ imagem: "assets/img/9gag.svg", usuario: "9gag" },
+        { imagem: "assets/img/meowed.svg", usuario: "meowed" },
+        { imagem: "assets/img/barked.svg", usuario: "barked" },
+        { imagem: "assets/img/nathanwpylestrangeplanet.svg", usuario: "nathanwpylestrangeplanet" },
+        { imagem: "assets/img/wawawicomics.svg", usuario: "wawawicomics" },
+        { imagem: "assets/img/respondeai.svg", usuario: "respondeai" },
+        { imagem: "assets/img/filomoderna.svg", usuario: "filomoderna" },
+        { imagem: "assets/img/memeriagourmet.svg", usuario: "memeriagourmet" }]
+
     return (
         <div class="stories">
-            <Story imagem="assets/img/9gag.svg" usuario="9gag"/>
-            <Story imagem="assets/img/meowed.svg" usuario="meowed"/>
-            <Story imagem="assets/img/barked.svg" usuario="barked"/>
-            <Story imagem="assets/img/nathanwpylestrangeplanet.svg" usuario="nathanwpylestrangeplanet"/>
-            <Story imagem="assets/img/wawawicomics.svg" usuario="wawawicomics"/>
-            <Story imagem="assets/img/respondeai.svg" usuario="respondeai"/>
-            <Story imagem="assets/img/filomoderna.svg" usuario="filomoderna"/>
-            <Story imagem="assets/img/memeriagourmet.svg" usuario="memeriagourmet"/>       
+            {stories.map((s) => <Story imagem={s.imagem} usuario={s.usuario} />)}
             <div class="setinha">
                 <ion-icon name="chevron-forward-circle"></ion-icon>
             </div>

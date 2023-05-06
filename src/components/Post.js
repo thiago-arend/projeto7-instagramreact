65447b4ff79fb3ff0e99dd4cf0af5c4fa0236d54
thiago-arend <thiago.arend@gmail.com>
2023-05-05 22:39:38 -0300

    save de emergencia

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,15 +5,24 @@ export default function Post(props) {
     const [heartColor, setHeartColor] = useState("");
     const [heartType, setHeartType] = useState("heart-outline");
     const [qtdLikes, setQtdLikes] = useState(0);
+    const [classeAnimacao, setClasseAnimacao] = useState("conteudo");
 
-    function aplicaEstiloHeart(efeito) {
+    // function aplicaEstiloHeart(efeito) {
+    //     if (heartType !== "heart") { // se o estilo não for heart, aplica heart
+    //         setHeartType("heart");
+    //         setHeartColor("vermelho");
+    //         setQtdLikes(qtdLikes + 1); // aumenta like
+
+    //         if (efeito)
+    //             console.log("aplica efeito!");
+    //     }
+    // }
+
+    function aplicaEstiloHeart() {
         if (heartType !== "heart") { // se o estilo não for heart, aplica heart
             setHeartType("heart");
             setHeartColor("vermelho");
             setQtdLikes(qtdLikes + 1); // aumenta like
-
-            if (efeito)
-                console.log("aplica efeito!");
         }
     }
 
@@ -30,41 +39,46 @@ export default function Post(props) {
         removeEstiloHeart();
     }
 
+    function aplicaAnimacaoHeart() {
+        aplicaEstiloHeart();
+        setClasseAnimacao("conteudo animacao");
+    }
+
     return (
         <div class="post">
             <div class="topo">
                 <div class="usuario">
-                <img src={props.topoImagem} alt={props.topoTitulo}/>
-                {props.topoTitulo}
+                    <img src={props.topoImagem} alt={props.topoTitulo} />
+                    {props.topoTitulo}
                 </div>
                 <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
+                    <ion-icon name="ellipsis-horizontal"></ion-icon>
                 </div>
             </div>
 
-            <div class="conteudo">
-                <img onDoubleClick={() => aplicaEstiloHeart(true)} onClick={() => aplicaEstiloHeart(false)}
-                    src={props.conteudoImagem} alt={props.conteudoAltImagem}/>
+            <div class={classeAnimacao}>
+                <img onClick={aplicaEstiloHeart} src={props.conteudoImagem}
+                    onDoubleClick={aplicaAnimacaoHeart} alt={props.conteudoAltImagem} />
             </div>
 
             <div class="fundo">
                 <div class="acoes">
-                <div>
-                    <ion-icon onClick={aplicaToggleHeart} class={heartColor} name={heartType}></ion-icon>
-                    <ion-icon name="chatbubble-outline"></ion-icon>
-                    <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                    <ion-icon onClick={() => (bookmark === "bookmark-outline") ? 
-                        setBookmark("bookmark") : setBookmark("bookmark-outline")} name={bookmark}></ion-icon>
-                </div>
+                    <div>
+                        <ion-icon onClick={aplicaToggleHeart} class={heartColor} name={heartType}></ion-icon>
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
+                    </div>
+                    <div>
+                        <ion-icon onClick={() => (bookmark === "bookmark-outline") ?
+                            setBookmark("bookmark") : setBookmark("bookmark-outline")} name={bookmark}></ion-icon>
+                    </div>
                 </div>
 
                 <div class="curtidas">
-                <img src={props.curtidasImagem} alt={props.curtidasNome}/>
-                <div class="texto">
-                    Curtido por <strong>{props.curtidasNome}</strong> e <strong>outras {qtdLikes} pessoas</strong>
-                </div>
+                    <img src={props.curtidasImagem} alt={props.curtidasNome} />
+                    <div class="texto">
+                        Curtido por <strong>{props.curtidasNome}</strong> e <strong>outras {qtdLikes} pessoas</strong>
+                    </div>
                 </div>
             </div>
         </div>
